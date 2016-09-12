7201927f01d28b6d3a277418cc674f9e6fd8eaf0
DD2XAlpha <danielalanis1997@outlook.com>
2016-09-11 22:02:36 -0500

    Download calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,10 @@
+puts " Introduce el primer valor"
+val1 = gets.chomp
+val1 = val1.to_i
+puts "Introduce el segundo valor"
+val2 = gets.chomp
+val2 = val2.to_i
+puts " ="
+puts val1 + val2
+puts "Pulsa enter para salir"
+salir = gets.chomp
