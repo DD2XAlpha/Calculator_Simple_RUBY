f514599233cbc7a5f07db4ed7e17b703c7736d59
DD2XALPHA <danielalanis1997@outlook.com>
2016-09-12 20:50:24 -0500

    partial update

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,32 @@
-puts " Enter value 1, then Intro"
+puts " ------------------------------Calculator--------------------------------------"
+puts " #                       DEVELOPED BY DD2XALPHA                           #"
+puts " ##############################################################################"
+puts " "
+puts " Signs of operation that can be used: +, -, *, x, /"
+puts " "
+puts " Introduce el primer valor"
 val1 = gets.chomp
 val1 = val1.to_i
-puts " Enter value 2, then Intro"
+puts " Introduce operacion"
+oper = gets.chomp
+puts " Introduce el segundo valor"
 val2 = gets.chomp
 val2 = val2.to_i
 puts " ="
-puts val1 + val2
-puts " Press Intro to exit."
+if oper == "+"
+     puts val1 + val2
+end
+if oper == "-"
+	puts val1 - val2
+end
+if oper == "*"
+	puts val1 * val2
+end
+if oper == "x"
+	puts val1 * val2
+end
+if oper == "/"
+	puts val1 / val2
+end
+puts "Pulsa enter para salir"
 salir = gets.chomp
