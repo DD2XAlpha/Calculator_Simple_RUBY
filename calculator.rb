0ec70632a40bf93c6b58cec8b1c616535fd7e818
DD2XALPHA <danielalanis1997@outlook.com>
2016-09-12 21:43:49 -0500

    Update to english

--- calculator.rb
+++ calculator.rb
@@ -4,12 +4,12 @@ puts " #########################################################################
 puts " "
 puts " Signs of operation that can be used: +, -, *, x, /"
 puts " "
-puts " Introduce el primer valor"
+puts " Enter first value"
 val1 = gets.chomp
 val1 = val1.to_i
-puts " Introduce operacion"
+puts " Enter operation"
 oper = gets.chomp
-puts " Introduce el segundo valor"
+puts " Enter second value"
 val2 = gets.chomp
 val2 = val2.to_i
 puts " ="
@@ -28,5 +28,5 @@ end
 if oper == "/"
 	puts val1 / val2
 end
-puts "Pulsa enter para salir"
-salir = gets.chomp
+puts "Press Intro to exit"
+close = gets.chomp
